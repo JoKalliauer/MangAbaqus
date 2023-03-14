82dbc16e77307c04f9cf27e273c355e5d3b7bc33
Johannes Kalliauer <johannes.kalliauer@tuwien.ac.at>
2023-03-14 12:46:19 -0400

    Update

--- gitpush.sh
+++ gitpush.sh
@@ -3,6 +3,17 @@
 #find . -path ./.git -prune -false -o -name '*' -size +1M
 find . -type d \( -path ./.git -o -path ./Output \) -prune -false -o -name '*' -size +150k #https://stackoverflow.com/a/4210072/6747994
 
+if [ -f *.owncloud ]; then
+    echo "Please download all files from owncloud before using git"
+    read -p "Do you wish to proceed?" yn
+    case $yn in
+        [Yy]* ) echo "not shure if thats a good choise";;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+fi
+
+
 git config --global core.eol lf
 git config core.eol lf
 git pull
